f41406df709f8b52bbada74fb57d94bfeadc3476
weshays <weshays@gbdev.com>
2012-03-25 23:07:17 -0700

    Fixed pathing

--- widget_info.php
+++ widget_info.php
@@ -4,7 +4,7 @@
     $otc_params[] = "subdomain_key=$subdomain_key";
     $otc_params[] = "plaza_display=$plaza_display";
     
-    $url = site_url() . '/wp-content/plugins/plaza-wordpress/load_data.php?'.implode('&', $otc_params); 
+    $url = site_url() . '/wp-content/plugins/the-city-plaza/load_data.php?'.implode('&', $otc_params); 
     echo file_get_contents($url);
   ?>
 </div>
